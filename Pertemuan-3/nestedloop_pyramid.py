c9365ee0a977c2326a63333c2fc444a36821a2df
adliaulia <59172520+adliaulia@users.noreply.github.com>
2021-04-23 00:02:14 +0700

    Pertemuan 3

--- Pertemuan-3/nestedloop_pyramid.py
+++ Pertemuan-3/nestedloop_pyramid.py
@@ -23,3 +23,16 @@ for i in range(3):
     for j in range(i):
         print("*", end="")
     print("*")
+
+# year = 2000
+
+# if(year%4==0):
+#     if(year%100==0):
+#         if(year%400==0):
+#             print("{0} is a leap year".format(year))
+#         else:
+#             print("{0} is not a leap year".format(year))
+#     else:
+#         print("{0} is a leap year".format(year))
+# else:
+#     print("{0} is not a leap year".format(year))
